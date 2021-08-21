bb5abd24e13fb8197f0e37e2f9a486ce9be816c5
Huu Khang Nguyen <52158153+huukhang1512@users.noreply.github.com>
2021-08-21 18:11:21 +1000

    change api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_API_URL = "https://or0k7bj0d3.execute-api.us-east-1.amazonaws.com/dev"
-export const WEBSOCKET_URL = "wss://rd5toyvfbl.execute-api.us-east-1.amazonaws.com/dev"
+export const BACKEND_API_URL = "https://t4692bryxh.execute-api.us-east-1.amazonaws.com/dev"
+export const WEBSOCKET_URL = "wss://41imu4ptaj.execute-api.us-east-1.amazonaws.com/dev"
